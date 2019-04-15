a3ffccd74b526c75f940ec8fd40aa02484018deb
FX-Wood <fxwood2@gmail.com>
2019-04-15 15:59:04 -0700

    Adding drive model

---
+++ models/drive.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const carSchema = new Schema({
+    seats: Number,
+    seatsTaken: Number,
+    shotgun: Boolean,
+    '4wd': Boolean,
+    awd: Boolean,
+    chains: Boolean,
+    roofrack: Boolean,
+    roofcargo: Boolean
+})
+
+mongoose.model('Car', carSchema)
+
+const driveSchema = new Schema({
+    user: {type: Schema.Types.ObjectId, ref: 'User'},
+    car: carSchema,
+    riders: [{type: Schema.Types.ObjectId, ref: 'User'}],
+    cost: Number,
+    goHour: Number,
+    goMin: Number,
+    goNote: String,
+    endHour: Number,
+    endMin: Number,
+    endNote: String,
+    note: String
+})
+
+module.exports = mongoose.model('Drive', driveSchema)
