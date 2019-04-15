4dfa44631112b2984bca1dff80030527540fc32e
FX-Wood <fxwood2@gmail.com>
2019-04-15 15:46:33 -0700

    Adding mountain schema

---
+++ models/mountain.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const mountainSchema = new Schema({
+    name: String,
+    website: String,
+    drives: [{type: Schema.Types.ObjectId, ref: 'Drive'}],
+    rides: [{type: Schema.Types.ObjectId, ref: 'Ride'}],
+    temperatureAtBase: Number
+})
+
+module.exports = mongoose.model('Mountain', mountainSchema)
