745f24ed63e4dee6aa80784fa65ddcbe57c1fc9d
FX-Wood <fxwood2@gmail.com>
2019-04-16 11:49:32 -0700

    Abstracted handle change function

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -1,45 +1,30 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import TextField from '@material-ui/core/TextField'
 
 export default class Signup extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            name: '',
+            first: '',
+            last: '',
             email: '',
             password: '',
-            message: ''
         }
-
-        this.handleNameChange = this.handleNameChange.bind(this)
-        this.handleEmailChange = this.handleEmailChange.bind(this)
-        this.handlePasswordChange = this.handlePasswordChange.bind(this)
+        this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
     }
 
-    handleNameChange(e) {
+    handleChange(e) {
         this.setState({
-            name: e.target.value
-        })
-    }
-    handleEmailChange(e) {
-        this.setState({
-            email: e.target.value
-        })
-    }
-    handlePasswordChange(e) {
-        this.setState({
-            password: e.target.value
+            [e.target.name]: e.target.value
         })
     }
     handleSubmit(e) {
         console.log('signing up...')
         e.preventDefault()
-        axios.post('/auth/signup', {
-            name: this.state.name,
-            email: this.state.email,
-            password: this.state.password
-        }).then( res => {
+        axios.post('/auth/signup', this.state)
+        .then( res => {
             console.log('res.data', res.data)
             if (res.data.type === 'error') {
                 console.log('error', res.data)
@@ -82,9 +67,10 @@ export default class Signup extends Component {
             <div className="signup">
                 <h3>Create a new account: </h3>
                 <form onSubmit={this.handleSubmit} >
-                    <input onChange={this.handleNameChange} value={this.state.name} type="text" name="name" placeholder="Enter your full name"/>
-                    <input onChange={this.handleEmailChange} value={this.state.email} type="email" name="email" placeholder="Enter your email address"/>
-                    <input onChange={this.handlePasswordChange} value={this.state.password} type="password" name="password" placeholder="Choose a password..."/>
+                    <input onChange={this.handleChange} value={this.state.first} type="text" name="name" placeholder="Enter your full name"/>
+                    <input onChange={this.handleChange} value={this.state.last} type="text" name="name" placeholder="Enter your full name"/>
+                    <input onChange={this.handleChange} value={this.state.email} type="email" name="email" placeholder="Enter your email address"/>
+                    <input onChange={this.handleChange} value={this.state.password} type="password" name="password" placeholder="Choose a password..."/>
                     <input type="submit" value="Sign Up!"/>
                 </form>
             </div>
