eb031e241ef674878e6110107bbe6016e24cad27
FX-Wood <fxwood2@gmail.com>
2019-04-18 15:16:39 -0700

    Making some tests

--- test/models.test.js
+++ test/models.test.js
@@ -1,3 +1,34 @@
-const should = require('chai').should
 const expect = require('chai').expect
-const db = require('../models')
+const should = require('chai').should()
+const mongoose = require('mongoose')
+
+
+mongoose.connect('mongodb://localhost/project4test', {useNewUrlParser: true});
+const db = mongoose.connection;
+
+describe('user model', async function() {
+    const User = require('../models/user')
+    it('user imports correctly', function() {
+        expect(typeof User).to.not.equal('undefined')
+    })
+    it('writes valid user to db', async function() {
+        const testUser = new User({
+            email: 'fx@fx.codes',
+            password: 'password()',
+        })
+        try {
+            await testUser.save()
+            let query = await User.findOne({where: {email:'fx@fx.codes'}}).exec()
+            console.log('query',query)
+            return Promise.resolve(expect(query.email).to.equal('fx@fx.codes'))
+        } catch(err) {
+            // return Promise.reject(err)
+        } 
+
+    })
+    // it('profile', function() {
+    //     expect(typeof user.profile).to.not.equal('undefined')
+    // })
+})
+
+mongoose.connection.close()
