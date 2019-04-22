2887a51e9479aca6cf134d76e0a5e6ea2440b0b9
FX-Wood <fxwood2@gmail.com>
2019-04-22 12:57:04 -0700

    Moving rideShow

---
+++ client/src/views/RideShow.jsx
@@ -0,0 +1,122 @@
+import axios from 'axios';
+import React, { Component } from 'react';
+import { withSnackbar } from 'notistack';
+import Button from '@material-ui/core/Button';
+import CloseIcon from '@material-ui/icons/Close';
+import EditIcon from '@material-ui/icons/Edit';
+import { Route, Link, withRouter } from 'react-router-dom';
+import RideFlow from '../views/RideFlow';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
+import RideUpdate from '../components/RideUpdate';
+
+const styles = theme => ({
+    root: {
+        maxWidth: '80%',
+        justifySelf: 'flexEnd',
+    },
+    flexibleIndicator: {
+        color: theme.palette.secondary.main
+    }
+})
+
+class RidesIndex extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            rides: [],
+            phoneNumber: '',
+        }
+    }
+
+    getRides = () => {
+        axios.get('/api/share')
+        .then(res => {
+            console.log('res', res)
+            this.setState({
+                rides: res.data.data,
+            })
+            this.props.enqueueSnackbar(JSON.stringify(res.data.message), {variant: 'success'})
+        })
+        .catch(err => {
+            console.log('err', err)
+            this.props.enqueueSnackbar(JSON.stringify(err.response), {variant: 'error'})
+        })
+    }
+
+    componentDidMount() {
+        console.log('rides index did mount')
+        this.getRides()
+    }
+
+    render() {
+        const { classes } = this.props
+        
+        const rides = this.state.rides.map((ride, i) => {
+            const start = new Date(ride.start)
+            const end = new Date(ride.end)
+            const { startFlex, endFlex } = ride
+            console.log('start', start)
+            console.log('end', end)
+            console.log({startFlex, endFlex})
+            return (
+                <Grid key={i} item>
+                    <Card style={{padding: '2em'}}>
+                        <Grid item><Typography variant="body1">{ride.note}</Typography></Grid>
+                        <Grid item container spacing={24}>
+                            <Grid item><Typography variant="body1">Date: {start.toLocaleDateString()}</Typography></Grid>
+                            <Grid item>
+                                <Typography variant="body1">
+                                    Start of day: {start.toLocaleTimeString()}
+                                    { startFlex && <span className={ classes.flexibleIndicator }> Flexible</span>}
+                                </Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="body1">
+                                    End of day: {end.toLocaleTimeString()}
+                                    { endFlex && <span className={ classes.flexibleIndicator } > Flexible</span>} 
+                                </Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="body1">
+                                    Contact phone: {this.state.phoneNumber}
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </Card>
+                </Grid>
+                )
+        })
+        // const routes = this.state.rides.map((ride, i) => {
+        //     const url = `/dash/ride/${ride._id}`
+        //     return (
+        //         <Route 
+        //             path={url}
+        //             render={() => {
+        //                 return (
+        //                     <RideUpdate rideID={ride._id} updateRide={this.updateRide}/>
+        //                 )
+        //             }} 
+        //             key={i}
+        //         />
+        //     )
+        // })
+
+        return (
+            <Grid container style={{padding: '80px'}} alignContent="center" justify="center" className={classes.root} spacing={40}>
+                {/* <Grid item>
+                    {routes}
+                </Grid> */}
+                <Grid item container spacing={24}>
+                    {rides}
+                </Grid>
+            </Grid>
+        )
+    }
+}
+
+
+export default withRouter(withStyles(styles)(withSnackbar(RidesIndex)))
